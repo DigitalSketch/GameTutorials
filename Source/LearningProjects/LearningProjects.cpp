be3b05651fab313e098d53f23d1d988257bf40ca
wjcarson2025 <william.carson@snhu.edu>
2026-08-26 11:51:52 -0400

    Initial Commit

---
+++ Source/LearningProjects/LearningProjects.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LearningProjects.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearningProjects, "LearningProjects" );
